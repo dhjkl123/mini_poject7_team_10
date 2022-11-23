3502cb526c2a5fc1a63051e334029244ba4aaa0b
egoing <jang728898@gmail.com>
2022-11-23 14:44:36 +0900

    test add

--- aivle-sign-language--/signlanguage/models.py
+++ aivle-sign-language--/signlanguage/models.py
@@ -7,6 +7,7 @@ class Result(models.Model):
     image = models.ImageField(blank=True)
     answer = models.CharField(max_length=10)
     result = models.CharField(max_length=10)
+    #테스트 결과용
     test = models.CharField(max_length=10)
     pub_date = models.DateTimeField('date published')
 
