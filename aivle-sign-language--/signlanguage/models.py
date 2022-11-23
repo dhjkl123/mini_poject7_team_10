fdfc63374caef02b6abf863f230512b63ceaeb94
yejig <110088882+yejig@users.noreply.github.com>
2022-11-23 12:21:52 +0900

    Update models.py
    is_selected change

--- aivle-sign-language--/signlanguage/models.py
+++ aivle-sign-language--/signlanguage/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db import transaction
+# from django.db.models import constraints
+# from django.db.models.query_utils import Q
 # from picklefield.fields import PickledObjectField
 
 # Create your models here.
@@ -14,7 +17,7 @@ class AI_Model(models.Model):
     # realModel = PickledObjectField()
     model_Name = models.CharField(max_length=50)
     model_Version = models.CharField(max_length=50, null=True)
-    model_File = models.FileField(upload_to='modelFiles/')
+    model_File = models.FileField(upload_to='model/')
     create_Date = models.DateField(auto_now=True)
     is_selected = models.BooleanField(default=True)
     
@@ -28,3 +31,15 @@ class AI_Model(models.Model):
     def __str__(self):
         return f'Name : {self.model_Name}, Date : {self.create_Date}'
 
+    # class Meta:
+    #     constraints = [
+    #         models.UniqueConstraint(fields=['is_selected'], condition=Q(is_selected=True), name='unique_is_selected')
+    #     ]
+    
+    def save(self, *args, **kwargs):
+        if not self.is_selected:
+            return super(AI_Model, self).save(*args, **kwargs)
+        with transaction.atomic():
+            AI_Model.objects.filter(
+                is_selected=True).update(is_selected=False)
+            return super(AI_Model, self).save(*args, **kwargs)
